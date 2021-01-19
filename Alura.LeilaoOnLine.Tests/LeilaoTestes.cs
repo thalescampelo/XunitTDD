992e80289c2b961ab02ed35631688e3137d10d1c
Thales Campelo <thalescampelo@gmail.com>
2021-01-19 18:08:38 -0300

    Iniciando o uso do projeto do xunit com implementação dos dois metodos de teste.

---
+++ Alura.LeilaoOnLine.Tests/LeilaoTestes.cs
@@ -0,0 +1,51 @@
+﻿using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnLine.Tests
+{
+    public class LeilaoTestes
+    {
+        [Fact]
+        private void LeilaoComVariosLances()
+        {
+            //Arrange - cenário do teste
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+            leilao.RecebeLance(maria, 900);
+            leilao.RecebeLance(fulano, 1000);
+            leilao.RecebeLance(maria, 990);
+
+            // Act - método sob teste
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 1000;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        [Fact]
+        private void LeilaoComApenasUmLance()
+        {
+            //Arrange - cenário do teste
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+            var maria = new Interessada("Maria", leilao);
+
+            leilao.RecebeLance(fulano, 800);
+
+            // Act - método sob teste
+            leilao.TerminaPregao();
+
+            // Assert
+            var valorEsperado = 800;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
