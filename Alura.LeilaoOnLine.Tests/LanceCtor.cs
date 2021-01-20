c50243adf7f46498d06a0854970e498cef307354
Thales Campelo <thalescampelo@gmail.com>
2021-01-20 11:42:40 -0300

    evolução dos testes ocom novos argumentos de excecao

---
+++ Alura.LeilaoOnLine.Tests/LanceCtor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnLine.Tests
+{
+    public class LanceCtor
+    {
+        [Fact]
+        private void LancaArgumentExeceptionDadoValorNegativo()
+        {
+            // Arrange
+            var valorNegativo = -100;
+
+            // Assert
+            Assert.Throws<ArgumentException>(
+                // Act
+                ()=> new Lance(null, valorNegativo)
+            );
+        }
+    }
+}
